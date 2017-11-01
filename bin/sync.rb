33a1c41aa1c60e3dfd815c4fd5af8c0b350d0f5a
wafrelka <wafrelka@gmail.com>
2017-11-02 01:59:38 +0900

    Add a script for data syncing

---
+++ bin/sync.rb
@@ -0,0 +1,31 @@
+$:.unshift(File.join(File.dirname(File.expand_path(__FILE__)), '..', 'app'))
+require 'logger'
+require 'ayari'
+
+
+if __FILE__ == $0
+
+	token = ENV['AYARI_TOKEN']
+	token_file = ENV['AYARI_TOKEN_FILE_PATH']
+	storage_path = ENV['AYARI_STORAGE_PATH'] || 'data'
+
+	if token.nil? && token_file.nil?
+		puts "Dropbox token not provided.\n"
+			"Set AYARI_TOKEN or AYARI_TOKEN_FILE_PATH env variable.\n"
+		exit
+	end
+
+	if token.nil?
+		token = File.binread(token_file).strip
+	end
+
+	storage = Ayari::LocalStorage.new(storage_path)
+
+	logger = Logger.new(STDOUT)
+	logger.level = Logger::INFO
+
+	sync = Ayari::Dropbox::Synchronizer.new(token, storage, logger)
+
+	sync.synchronize
+
+end

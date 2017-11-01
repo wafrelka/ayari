f110ca419453e96ee90762e4f11de6de885c4ea7
wafrelka <wafrelka@gmail.com>
2017-11-02 00:52:16 +0900

    Remove unnecessary require

--- spec/rspec_helper.rb
+++ spec/rspec_helper.rb
@@ -1,6 +1,5 @@
 $:.unshift(File.join(File.dirname(File.expand_path(__FILE__)), '..', 'app'))
 require 'rspec'
-require 'rspec/expectations'
 
 RSpec.configure do |c|
 	c.filter_run_excluding internet_required: true

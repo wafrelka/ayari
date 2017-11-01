a4cecc1a60b95fa10b52e8af37a8c8e343dccb9c
wafrelka <wafrelka@gmail.com>
2017-11-02 00:55:53 +0900

    Add some `require`s to 'app/ayari.rb' for convenience

--- app/ayari.rb
+++ app/ayari.rb
@@ -1,1 +1,3 @@
 require 'ayari/router'
+require 'ayari/local_storage'
+require 'ayari/dropbox'
---
+++ app/ayari/dropbox.rb
@@ -0,0 +1,3 @@
+require 'ayari/dropbox/hasher'
+require 'ayari/dropbox/client'
+require 'ayari/dropbox/synchronizer'

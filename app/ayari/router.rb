fd17a54af3619fe1aefed7813cbf499a7b5dc663
wafrelka <wafrelka@gmail.com>
2018-10-07 02:35:21 +0900

    Implement `path_to` and `get_content` helpers to improve partial rendering support

--- app/ayari/router.rb
+++ app/ayari/router.rb
@@ -23,20 +23,47 @@ module Ayari
 
 		helpers do
 
-			def render_haml(remote_path, locals={})
+			def path_to(path)
+				RoutingRules.get_remote_path(path, @rendering_path)
+			end
+
+			def get_content(remote_path, locals={})
 
 				obj = settings.storage.get_object_info(remote_path)
-				if obj.nil?
-					raise Sinatra::NotFound
-				end
+				return nil if obj.nil?
 
 				begin
 					txt = File.read(obj.local_path, encoding: ENCODING)
 				rescue Errno::NOENT
-					raise Sinatra::NotFound
+					return nil
 				end
 
-				haml txt, locals: locals
+				txt
+
+			end
+
+			def render_content(ty, remote_path, locals={})
+
+				content = get_content(remote_path, locals)
+				raise Sinatra::NotFound if content.nil?
+
+				case ty
+				when :md
+
+					template_path, opts = Ayari::Markdown::Processor::process_text(content)
+					template_remote_path = RoutingRules.get_remote_path(template_path, remote_path)
+					render_content(:haml, template_remote_path, opts.merge(locals))
+
+				when :haml
+
+					@rendering_path = remote_path
+					haml content, locals: locals
+
+				else
+
+					raise Sinatra::NotFound
+
+				end
 
 			end
 
@@ -60,40 +87,20 @@ module Ayari
 				.map{ |c| [storage.get_object_info(c), File.basename(c)] }
 				.find{ |obj, fname| ! obj.nil? }
 
-			if selected.nil?
-				raise Sinatra::NotFound
-			end
+			raise Sinatra::NotFound if selected.nil?
 
 			obj, fname = selected
 			ext = File.extname(fname)
 
 			case ext
 			when '.md'
-
-				begin
-					txt = File.read(obj.local_path, encoding: ENCODING)
-				rescue Errno::NOENT
-					raise Sinatra::NotFound
-				end
-
-				template_rel_path, opts = Ayari::Markdown::Processor::process_text(txt)
-				template_path = File.absolute_path(template_rel_path, File.dirname(obj.remote_path))
-				render_haml(template_path, opts)
-
+				render_content(:md, obj.remote_path)
 			when '.haml'
-
-				render_haml(obj.remote_path)
-
+				render_content(:haml, obj.remote_path)
 			else
-
 				types = MIME::Types.type_for(fname)
-
-				if types.length > 0
-					content_type types[0].to_s
-				end
-
+				content_type types[0].to_s if ! types.empty?
 				send_file File.absolute_path(obj.local_path)
-
 			end
 
 		end
